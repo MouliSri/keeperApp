a25ea2158bf493d0441789a9a959926574bd6bcd
Mouli S <104787111+MouliSri@users.noreply.github.com>
2022-12-25 16:27:18 +0530

    Initial commit

---
+++ src/notes.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function notes() {
+  return (
+    <div className="note">
+      <h1>Daily Routine</h1>
+      <p>Walking ,jumping ,swimming</p>
+    </div>
+  );
+}
